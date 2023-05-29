9bfef3f15cb4a9bd6fca56e3e8ced3e77e9e21d9
shmuelazriel <shmuelazriel2@gmail.com>
2023-05-29 14:57:36 +0300

    basic DB not connected

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -34,21 +34,18 @@ def products():
 @app.route('/submit_product', methods=['POST'])
 def submit_product():
     name = request.form.get('name')
-    price = request.form.get('price')
+    price = int(request.form.get('price'))
 
-    # Connect to the database
     conn = sqlite3.connect('your_database.db')
     cursor = conn.cursor()
 
-    # Insert the submitted data into the products table
     cursor.execute("INSERT INTO products (name, price) VALUES (?, ?)", (name, price))
 
-    # Commit the changes and close the connection
     conn.commit()
-    cursor.close()
     conn.close()
 
-    return 'Product submitted successfully!'
+    return redirect(url_for('products'))
+
 
 if __name__ == '__main__':
     app.run()
